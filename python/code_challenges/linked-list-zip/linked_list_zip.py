2acc5e81608a15beef5d9355f0995b654e81e8f0
Brannon Starnes <brannlee808@gmail.com>
2021-12-07 05:54:50 -0800

    adds README, whiteboard img, and linked_list_zip.py

---
+++ python/code_challenges/linked-list-zip/linked_list_zip.py
@@ -0,0 +1,42 @@
+if __name__ is __main__:
+
+    class Node():
+
+        def __init__(self, value, next = None):
+            self.value = value
+            self.next = next
+
+    class LinkedList():
+
+        def __init__(self, head = None):
+            self.head = head
+
+        def insert(self, value):
+            self.head = Node(value, self.head)
+
+
+    def zip_lists(list_1, list_2):
+        current_1 = list_1.head
+        current_2 = list_2.head
+
+        while current_2:
+            temp = current_1.next
+            current_1.next = Node(current_2.value, temp)
+            current_1 = current_1.next.next #(does this work?)
+            if current_2.next == True:
+                current_2 = current_2.next
+            else:
+                print(list_1)
+                return list_1
+
+    list1 = LinkedList()
+    list1.insert("C")
+    list1.insert("B")
+    list1.insert("A")
+
+    list2 = LinkedList()
+    list2.insert("3")
+    list2.insert("2")
+    list2.insert("1")
+
+    zip_lists(list1, list2)
