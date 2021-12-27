8e1c49376301c815e12c618f54092316bd31bc1c
Brannon Starnes <brannlee808@gmail.com>
2021-12-26 21:50:22 -0800

    adds tests for linked_list_zip

--- python/code_challenges/linked_list_zip/linked_list_zip.py
+++ python/code_challenges/linked_list_zip/linked_list_zip.py
@@ -1,42 +1,45 @@
-if __name__ is __main__:
+# if __name__ == "__main__" :
 
-    class Node():
+class Node():
 
-        def __init__(self, value, next = None):
-            self.value = value
-            self.next = next
+    def __init__(self, value, next = None):
+        self.value = value
+        self.next = next
 
-    class LinkedList():
+class LinkedList():
 
-        def __init__(self, head = None):
-            self.head = head
+    def __init__(self, head = None):
+        self.head = head
 
-        def insert(self, value):
-            self.head = Node(value, self.head)
+    def insert(self, value):
+        self.head = Node(value, self.head)
 
 
-    def zip_lists(list_1, list_2):
-        current_1 = list_1.head
-        current_2 = list_2.head
+def zip_lists(list_1, list_2):
+    current_1 = list_1.head
+    current_2 = list_2.head
 
-        while current_2:
-            temp = current_1.next
-            current_1.next = Node(current_2.value, temp)
-            current_1 = current_1.next.next #(does this work?)
-            if current_2.next == True:
-                current_2 = current_2.next
-            else:
-                print(list_1)
-                return list_1
+    if current_1 is None or current_2 is None:
+        raise Exception(ValueError)
 
-    list1 = LinkedList()
-    list1.insert("C")
-    list1.insert("B")
-    list1.insert("A")
+    while current_2:
+        temp = current_1.next
+        current_1.next = Node(current_2.value, temp)
+        current_1 = current_1.next.next #(does this work?)
+        if current_2.next:
+            current_2 = current_2.next
+        else:
+            return list_1
 
-    list2 = LinkedList()
-    list2.insert("3")
-    list2.insert("2")
-    list2.insert("1")
+# list1 = LinkedList()
+# list1.insert("C")
+# list1.insert("B")
+# list1.insert("A")
 
-    zip_lists(list1, list2)
+# list2 = LinkedList()
+# list2.insert("3")
+# list2.insert("2")
+# list2.insert("1")
+
+# zipped = zip_lists(list1, list2)
+# print(zipped.head.value, zipped.head.next.value, zipped.head.next.next.value, zipped.head.next.next.next.value, zipped.head.next.next.next.next.value, zipped.head.next.next.next.next.next.value)
